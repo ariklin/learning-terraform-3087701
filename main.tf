e73bc809f204d7e15cea10eef4d0a435c33ba30b
ariklin <ariklin@microsoft.com>
2023-06-04 18:48:59 +0300

    33

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -34,9 +34,8 @@ module "vpc" {
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id              = module.vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
@@ -47,8 +46,8 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
+  vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
-  vpc_id  = module.vpc.public_subnets[0]
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
