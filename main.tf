c22d9b535601a34b9b1fd3adcb2087f12f7d0689
ariklin <ariklin@microsoft.com>
2023-06-04 17:58:14 +0300

    11

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ resource "aws_instance" "blog" {
   instance_type          = var.instance_type
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
@@ -47,7 +48,7 @@ module "blog_sg" {
   version = "4.13.0"
 
   name    = "blog"
-  vpc_id  = module.vpc.public_subnets[0]
+  vpc_id  = module.vpc.vpc_id
 
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
