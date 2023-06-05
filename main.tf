5b62d4e7e30fdd2c6c40ea238d7573064bf7b017
ariklin <ariklin@microsoft.com>
2023-06-05 12:53:41 +0300

    autoscaling

--- main.tf
+++ main.tf
@@ -29,18 +29,42 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+# resource "aws_instance" "blog" {
+#   ami                    = data.aws_ami.app_ami.id
+#   instance_type          = var.instance_type
+#   subnet_id              = module.blog_vpc.public_subnets[0]
+#   vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+#   tags = {
+#     Name = "Learning Terraform"
+#   }
+# }
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.10.0"
+
+  name = "blog"
+  min_size            = 1
+  max_size            = 2
+
+vpc_zone_identifier = module.blog_vpc.public_subnets
+target_group_arns   = module.blog_alb.target_group_arns
+security_groups = [module.blog_sg.security_group_id]
+
+image_id         = data.aws_ami.app_ami.id
+instance_type    = var.instance_type
+
+  health_check_type   = "EC2"
+  force_delete        = true
 
   tags = {
-    Name = "Learning Terraform"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
