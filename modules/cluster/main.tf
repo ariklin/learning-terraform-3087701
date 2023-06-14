c4c07fc534bb5dcd2b3658598a54700af3871ae5
ariklin <ariklin@microsoft.com>
2023-06-14 19:03:10 +0300

    1

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -61,37 +61,3 @@ module "explore-california-vpc" {
   enable_dns_support = true
   enable_dns_hostnames = true
 }
-
-module "explore-california-cluster" {
-  source          = "./."
-  cluster_name    = "explore-california-cluster"
-  cluster_version = "1.20"
-  subnets          = module.explore-california-vpc.public_subnets
-  vpc_id          = module.explore-california-vpc.vpc_id
-  worker_groups = [
-    {
-      instance_type = "t3.medium"
-      asg_max_size  = 5
-      spot_price = "0.02"
-      additional_security_group_ids = [ aws_security_group.enable_ssh.id ]
-      kubelet_extra_args = "--node-labels=node.kubernetes.io/lifecycle=spot"
-      suspended_processes = ["AZRebalance"]
-    },
-    {
-      instance_type = "t3.large"
-      asg_max_size  = 5
-      spot_price = "0.03"
-      additional_security_group_ids = [ aws_security_group.enable_ssh.id ]
-      kubelet_extra_args = "--node-labels=node.kubernetes.io/lifecycle=spot"
-      suspended_processes = ["AZRebalance"]
-    }
-  ]
-}
-
-resource "aws_ecr_repository" "explore-california" {
-  name = "explore-california"
-  image_tag_mutability = "MUTABLE"
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
