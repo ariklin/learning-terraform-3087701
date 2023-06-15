2ce6b4d6a3d8a5995aed7fe98a74041ece50f335
ariklin <ariklin@microsoft.com>
2023-06-15 19:39:50 +0300

    gdfgdgdg

--- infra/main.tf
+++ infra/main.tf
@@ -75,7 +75,7 @@ module "explore-california-cluster" {
   version = "19.15.3"
   cluster_name =   "explore-california"
   cluster_version = "1.20"
-  subnets_subnet_ids = module.explore-california-vpc.public_subnets
+  subnet_ids = module.explore-california-vpc.public_subnets
   vpc_id          = module.explore-california-vpc.vpc_id
   worker_groups = [
     {
